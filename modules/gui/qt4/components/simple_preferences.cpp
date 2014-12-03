af9c39d946862267f1882eecde24050ec471138f
Jean-Baptiste Kempf <jb@videolan.org>
2014-12-03 12:30:32 +0100

    Qt: fix compilation

--- modules/gui/qt4/components/simple_preferences.cpp
+++ modules/gui/qt4/components/simple_preferences.cpp
@@ -168,7 +168,7 @@ static const char *const ppsz_language_text[] =
     "ಕನ್ನಡ",
     "한국어",
     "lietuvių",
-    "latviešu valoda'
+    "latviešu valoda",
     "Монгол хэл",
     "मराठी",
     "Melayu",
