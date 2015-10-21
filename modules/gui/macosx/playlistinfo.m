dcc4f9f918b9caf70ab973dee72204a3a5a85d24
David Fuhrmann <dfuhrmann@videolan.org>
2015-10-21 22:39:02 +0200

    macosx: fix error message for media information panel
    not cherry picked from 10145e039042c1a15e92b47d7e88f1d632d9c6a1

--- modules/gui/macosx/playlistinfo.m
+++ modules/gui/macosx/playlistinfo.m
@@ -363,9 +363,9 @@ static VLCInfo *_o_sharedInstance = nil;
     return;
 
 error:
-    NSRunAlertPanel(_NS("Error while saving meta"),
-        @"%@",_NS("VLC was unable to save the meta data."),
-        _NS("OK"), nil, nil);
+    NSRunAlertPanel(_NS("Error while saving meta"), @"%@",
+                    _NS("OK"), nil, nil,
+                    _NS("VLC was unable to save the meta data."));
 }
 
 - (IBAction)downloadCoverArt:(id)sender
