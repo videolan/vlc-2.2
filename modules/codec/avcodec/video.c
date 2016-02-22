videolan/vlc

commit b71c85b3d88b8d0ad2d4a63bf58ebcd2ad771cbf

fix video playback when width and height differ from coded_width and coded_height
Signed-off-by: Rémi Denis-Courmont <remi@remlab.net>

modules/codec/avcodec/video.c
@@ -125,8 +125,14 @@ static inline picture_t *ffmpeg_NewPictBuf( decoder_t *p_dec,
 {
     decoder_sys_t *p_sys = p_dec->p_sys;
 
-    p_dec->fmt_out.video.i_width = p_context->width;
-    p_dec->fmt_out.video.i_height = p_context->height;
+    if( p_context->coded_width != p_context->width ||
+        p_context->coded_height != p_context->height )
+    {
+        p_dec->fmt_out.video.i_visible_width = p_context->width;
+        p_dec->fmt_out.video.i_visible_height = p_context->height;
+    }
+    p_dec->fmt_out.video.i_width = p_context->coded_width;
+    p_dec->fmt_out.video.i_height = p_context->coded_height;
 
     if( !p_context->width || !p_context->height )
     {

videolan/vlc-2.2

commit bdd50cc0d7c316d123974ca9ae35c02f550fdc35

avcodec: pass consistent dimensions to hardware decoder
Commit 01c4f44e88bfbb8ecf108c0fe40f3797bddac686 fixed one call site of
vlc_va_Setup() but left the other one silently broken. This should fix
decoding with libvdpau-va-gl (untested).

The original regression was introduced by
commit b71c85b3d88b8d0ad2d4a63bf58ebcd2ad771cbf.

modules/codec/avcodec/video.c
@@ -1359,10 +1359,10 @@ static enum PixelFormat ffmpeg_GetFormat( AVCodecContext *p_context,
 
         /* We try to call vlc_va_Setup when possible to detect errors when
          * possible (later is too late) */
-        if( p_context->width > 0 && p_context->height > 0
+        if( p_context->coded_width > 0 && p_context->coded_height > 0
          && vlc_va_Setup( p_va, &p_context->hwaccel_context,
                           &p_dec->fmt_out.video.i_chroma,
-                          p_context->width, p_context->height ) )
+                          p_context->coded_width, p_context->coded_height ) )
         {
             msg_Err( p_dec, "acceleration setup failure" );
             break;