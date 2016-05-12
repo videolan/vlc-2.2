db521592b7300555c4c6d265735622049b015974
Benoit du Payrat <benoit@videolabs.io>
2016-05-12 10:32:43 +0200

    Qt: Leaves minimal view on quit to prevent size issues on start
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>
    (cherry picked from commit bf2a7faff9f804cc7c65b72e3714de02910628b7)
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- modules/gui/qt4/main_interface.cpp
+++ modules/gui/qt4/main_interface.cpp
@@ -1532,6 +1532,8 @@ void MainInterface::wheelEvent( QWheelEvent *e )
 void MainInterface::closeEvent( QCloseEvent *e )
 {
 //  hide();
+    if ( b_minimalView )
+        setMinimalView( false );
     emit askToQuit(); /* ask THEDP to quit, so we have a unique method */
     /* Accept session quit. Otherwise we break the desktop mamager. */
     e->accept();
