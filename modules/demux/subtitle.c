aef4ee7f141439921d31ea21bc5700436cb3fb51
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2017-04-28 10:00:11 +0200

    subtitle: Fix potential heap buffer overflow
    Reported-by: Yannay Livneh <yannayl@checkpoint.com>
                 Omri Herscovici <omrih@checkpoint.com>
                 Omer Gull <omergu@checkpoint.com>
    
    (cherry picked from commit 611398fc8d32f3fe4331f60b220c52ba3557beaa)
    Signed-off-by: Hugo Beauzée-Luyssen <hugo@beauzee.fr>

--- modules/demux/subtitle.c
+++ modules/demux/subtitle.c
@@ -1687,7 +1687,8 @@ static int ParseJSS( demux_t *p_demux, subtitle_t *p_subtitle, int i_idx )
         if( !s )
             return VLC_EGENERIC;
 
-        psz_orig = malloc( strlen( s ) + 1 );
+        size_t line_length = strlen( s );
+        psz_orig = malloc( line_length + 1 );
         if( !psz_orig )
             return VLC_ENOMEM;
         psz_text = psz_orig;
@@ -1727,6 +1728,8 @@ static int ParseJSS( demux_t *p_demux, subtitle_t *p_subtitle, int i_idx )
             {
             case 'S':
                  shift = isalpha( (unsigned char)psz_text[2] ) ? 6 : 2 ;
+                 if ( shift > line_length )
+                     continue;
 
                  if( sscanf( &psz_text[shift], "%d", &h ) )
                  {
@@ -1764,6 +1767,8 @@ static int ParseJSS( demux_t *p_demux, subtitle_t *p_subtitle, int i_idx )
 
             case 'T':
                 shift = isalpha( (unsigned char)psz_text[2] ) ? 8 : 2 ;
+                if ( shift > line_length )
+                    continue;
 
                 sscanf( &psz_text[shift], "%d", &p_sys->jss.i_time_resolution );
                 break;
